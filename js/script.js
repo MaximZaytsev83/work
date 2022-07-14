784cf78dcf7b341e8c540da09f20509c2f5ae523
Maxim <zaytsev3@rambler.ru>
2022-07-14 21:58:22 +0300

    first edit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log (--decr);
+
+
+const isChecked = true,
+    isClosed = false;
+
+console.log(isChecked || isClosed);
